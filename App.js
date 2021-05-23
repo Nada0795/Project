c700ba53aa2ad22a265e393f4d546f69a38a29b9
Nada0795 <nadakh1993@icloud.com>
2021-05-24 01:38:22 +0300

    update the file

---
+++ App.js
@@ -0,0 +1,32 @@
+
+
+// prompt('whats your favorite Color of flowers ? yellow or red ?')
+
+// prompt("whats type of flowers you want ? DahliaIn , Aster, Delphinium ,Calla Lily")
+
+// prompt('How many flowers do you want to buy ?')
+
+// prompt('whats your favorite type ?DahliaIn or Aster ?')
+
+
+    
+let userColor = prompt('whats your favorite Color of flowers ? yellow or red ?')
+console.log(userColor);
+
+
+let flowerType = prompt("whats type of flowers you want ? DahliaIn , Aster, Delphinium ,Calla Lily")
+alert ('yes,it is '+flowerType);
+
+
+
+const userFlower = prompt('How many flowers do you want to buy ?')
+console.log(userFlower);
+
+
+let favoriteType= prompt('whats your favorite type ?Calla Lily or Aster ?');
+
+alert('DONE ')
+alert( 'your favorite color is '+userColor+' and you want '+flowerType+'  type '+' and you want to buy '+userFlower+' of flowers');
+
+confirm("THANK YOU For Using Our Site")
+
